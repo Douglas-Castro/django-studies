44a92cfe668e2e93a58408bb80f861a0b609f18a
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-02 20:44:10 -0300

    :arrow_up: Upgrade contact view with form info

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import contactForms
 
 
 def index(request):
@@ -6,7 +8,31 @@ def index(request):
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    form = contactForms(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            print('Mensagem Enviada!')
+            print(f'Nome: {name}')
+            print(f'E-mail: {email}')
+            print(f'Assunto: {subject}')
+            print(f'Mensagem: {message}')
+
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = contactForms()
+        else:
+            messages.error(request, 'Erro ao enviar e-mail!')
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'contact.html', context)
 
 
 def product(request):
