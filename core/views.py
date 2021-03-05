8cce91529c89a2e8c3f3d905dd2b3d9b5a529ed9
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-05 13:04:27 -0300

    :arrow_up: Update index view

--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import contactForms, ProductModelForm
-
+from .models import Product
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
 def contact(request):
